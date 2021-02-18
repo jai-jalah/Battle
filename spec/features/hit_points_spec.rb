141f5bc889c746cd05ff8b2067ae6a1728a5168f
Adam Jonas <adamjonas25@gmail.com>
2021-02-18 16:06:03 +0000

    add spec helper methods

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'Player hit points' do
     scenario "see player 2's Hit Points" do
-        visit '/'
-        fill_in :p1_name, with: 'Jai'
-        fill_in :p2_name, with: 'Adam'
-        click_button 'Submit'
+        sign_in_and_play
         expect(page).to have_content "Adam's Hit Points: 50"
     end
 end
--- spec/features/start_form_spec.rb
+++ spec/features/start_form_spec.rb
@@ -1,9 +1,6 @@
 feature 'Form for player names' do
     scenario 'enter & submit player names and see them on screen' do
-        visit '/'
-        fill_in :p1_name, with: 'Jai'
-        fill_in :p2_name, with: 'Adam'
-        click_button 'Submit'
+        sign_in_and_play
         expect(page).to have_content 'Jai vs Adam'
     end
 end
