02413c7306e11ee1bd39179691c3649f89ee790f
jai-jk <jay.jk@hotmail.com>
2021-02-18 15:21:45 +0000

    Close #1 user story - complete feature test and code

--- app.rb
+++ app.rb
@@ -6,11 +6,10 @@ class Battle < Sinatra::Base
     end
 
     post '/names' do
-        p params
-        @player_name = params[:name]
+        @p1_name = params[:p1_name]
+        @p2_name = params[:p2_name]
         erb :play
     end
-
 
     run! if app_file == $0
 end
